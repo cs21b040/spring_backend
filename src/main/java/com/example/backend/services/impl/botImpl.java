dcf798474da1e00dc10147956526c85a5d3beeb7
pavan <cs21b040@iittp.ac.in>
2024-05-29 17:47:43 +0530

    transliteration added

--- src/main/java/com/example/backend/services/impl/botImpl.java
+++ src/main/java/com/example/backend/services/impl/botImpl.java
@@ -1,14 +1,21 @@
 package com.example.backend.services.impl;
 import org.springframework.stereotype.Service;
 
+import com.anyascii.AnyAscii;
 import com.example.backend.services.botService;
 
 import lombok.AllArgsConstructor;
 
 @Service
 @AllArgsConstructor
-public class botImpl implements botService {@Override
+public class botImpl implements botService {
+    @Override
     public String getResponse(String msg) {
-        return "From Bot : "+msg;
-    }    
+        return "From Bot : "+transliterateString(msg);
+    }
+    public String transliterateString(String msg) {
+        String s = AnyAscii.transliterate(msg);
+        System.out.println(s);
+        return s;
+    }
 }
